7386cd99347171fbbdcbad9ba9b340d5db6f6a15
James Gomis <jamesislike@gmail.com>
2017-11-18 14:23:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ function BoardMember (name, homeState,training){
   this.veto = function(){
     return `No, I must disagree`
   }
+  this.approve = function(){
+    return `You can do that!`
+  }
   this.doCharity = function(){
-    `I like to help people.`
+      return `I like to help people.`
+  }
+  this.releasePressStatement = function(){
+    return `You will see great things from Scuber.`
+  }
+  this.sayHi = function(){
+    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
   }
 }
   //this.veto = function(){   annonomous function - used for every Object and it is not reccomended
