fa33327a2554a9e675ce77b24e64fc9f93b659c3
James Gomis <jamesislike@gmail.com>
2017-11-18 14:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ function BoardMember (name, homeState,training){
   this.name = name
   this.homeState = homeState
   this.training = training
+  this.veto = function(){
+    return `No, I must disagree`
+  }
 }
   //this.veto = function(){   annonomous function - used for every Object and it is not reccomended
 
 
-BoardMember.prototype.veto = function{
-  console.log(`No, I must disagree`)
-}
-
-BoardMember.prototype.doCharity = function(){
-  returns ``
-}
-
-BoardMember.prototype.releasePressStatement = function(){
-  returns ``
-}
+// BoardMember.prototype.veto = function{
+//   console.log(`No, I must disagree`)
+// }
+//
+// BoardMember.prototype.doCharity = function(){
+//   returns ``
+// }
+//
+// BoardMember.prototype.releasePressStatement = function(){
+//   returns ``
+// }
